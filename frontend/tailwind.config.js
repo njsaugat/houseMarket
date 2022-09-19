70e7cb2848d1b7d3aea55424a5aad099d834679f
Saugat Poudel <njsaugat@gmail.com>
2022-09-19 12:15:17 +0545

    tailwind file added

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'blue-green': '#89D4CF',
+        'majorelle-blue': '#734AE8',
+      },
+    },
+  },
+  plugins: [],
+};
