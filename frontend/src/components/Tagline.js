714497e37295eb7662b2408daef1fbe1fbb38557
Saugat Poudel <njsaugat@gmail.com>
2022-09-22 14:41:23 +0545

    tagline added

---
+++ frontend/src/components/Tagline.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Tagline = ({ textSize }) => {
+  return (
+    <section
+      className={`text-5xl leading-normal md:text-${textSize} md:leading-normal my-10 font-bold `}
+    >
+      Your Dream
+      <br />
+      <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-cyan-300">
+        House is Waiting
+      </span>
+      <br />
+      For You. <br />
+    </section>
+  );
+};
+
+export default Tagline;
