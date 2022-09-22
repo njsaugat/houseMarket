b754c8dc36b0521a3e787774ea7fc25770551d98
Saugat Poudel <njsaugat@gmail.com>
2022-09-22 14:41:46 +0545

    hero updated

--- frontend/src/LandingPage/Hero.js
+++ frontend/src/LandingPage/Hero.js
@@ -1,18 +1,11 @@
 import React from 'react';
+import Tagline from '../components/Tagline';
 import house from '../house.png';
 const Hero = () => {
   return (
     <main className="w-screen p-10 md:p-20 relative md:flex md:justify-between items-center overflow-hidden   mb-32  bg-gradient-to-r from-gray-200 to-gray-50 ">
       <div className="left my-24 mb-10  md:w-3/5">
-        <section className="text-5xl leading-normal md:text-7xl md:leading-normal my-10 font-bold ">
-          Your Dream
-          <br />
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-cyan-300">
-            House is Waiting
-          </span>
-          <br />
-          For You. <br />
-        </section>
+        <Tagline textSize={'7xl'} />
         <section className="description text-gray-400 md:w-1/2">
           Our team of experts uses a methodology to identify the credit cards
           most likely to fit your needs. We examine annual percentage rates,
