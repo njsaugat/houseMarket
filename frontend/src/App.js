1c5df47dcff04042f43d5be783db055ad1773c34
Saugat Poudel <njsaugat@gmail.com>
2022-09-21 20:45:01 -0700

    app file added

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from './LandingPage/LandingPage';
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
