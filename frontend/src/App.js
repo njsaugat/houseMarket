1a7be80223c5a3959ee18e1c0f7289e2be9f8705
Saugat Poudel <njsaugat@gmail.com>
2022-09-22 14:40:24 +0545

    app js file added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import LandingPage from './LandingPage/LandingPage';
-
+import { Route, Routes } from 'react-router-dom';
+import Signup from './Login-signup/Signup';
 function App() {
   return (
     <>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 }
