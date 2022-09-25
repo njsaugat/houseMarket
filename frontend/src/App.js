e12ed41602ed6d9e3ee5d6815ea28a02997b0148
Saugat Poudel <njsaugat@gmail.com>
2022-09-25 16:21:23 +0545

    app file updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import LandingPage from './LandingPage/LandingPage';
 import { Route, Routes } from 'react-router-dom';
 import Signup from './Login-signup/Signup';
+import Login from './Login-signup/Login';
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
