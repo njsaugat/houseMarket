9dcf426cbf9e9be889c1592664946ec87eea26e4
Saugat Poudel <njsaugat@gmail.com>
2022-09-24 23:26:26 -0600

    check password added

--- frontend/src/Login-signup/checkConfPassword.js
+++ frontend/src/Login-signup/checkConfPassword.js
@@ -12,3 +12,18 @@ export function checkConfPassword(confPassword, password) {
     return true;
   }
 }
+
+function callInvalid(element) {
+  element.classList.add('invalid');
+}
+
+function callValid(element) {
+  element.classList.add('valid');
+}
+
+function createError(element, message) {
+  const span = document.createElement('span');
+  span.innerText = message;
+  span.classList.add('error');
+  element.parentNode.appendChild(span);
+}
