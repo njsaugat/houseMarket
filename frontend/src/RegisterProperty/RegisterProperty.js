4d6b09fdeed4f0bba59fe2bc173b7c39a09fd11b
Saugat Poudel <njsaugat@gmail.com>
2022-09-29 13:35:31 +0545

    changes made

--- frontend/src/RegisterProperty/RegisterProperty.js
+++ frontend/src/RegisterProperty/RegisterProperty.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const RegisterProperty = () => {
-  return <div>RegisterProperty</div>;
+  return (
+    <div>
+      <div className="left"></div>
+      <div className="right">
+        <form action=""></form>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterProperty;
