8ca2d6e7361b858b6991be20f828aa4fb7732c97
Saugat Poudel <njsaugat@gmail.com>
2022-09-29 18:10:57 +0545

    app updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import LandingPage from './LandingPage/LandingPage';
 import { Route, Routes } from 'react-router-dom';
 import Signup from './Login-signup/Signup';
 import Login from './Login-signup/Login';
+import RegisterProperty from './RegisterProperty/RegisterProperty';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register-property" element={<RegisterProperty />} />
       </Routes>
     </>
   );
