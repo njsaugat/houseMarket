b0cb265b272071363165d0ae017cbe4720fca4db
Saugat Poudel <njsaugat@gmail.com>
2022-10-03 13:14:39 +0545

    controller file updated

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
 // Home page route.
-router.get("/", function (req, res) {
-  
+router.get('/', function (req, res) {
+  res.send('Wiki home page');
+});
