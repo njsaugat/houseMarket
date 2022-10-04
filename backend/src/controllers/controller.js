4c9f1906ba920dba6e18b641ee56578262faf517
Saugat Poudel <njsaugat@gmail.com>
2022-10-04 11:17:57 +0545

    controller added

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -49,3 +49,8 @@ exports.author_create_post = (req, res) => {
 exports.author_delete_get = (req, res) => {
   res.send('NOT IMPLEMENTED: Author delete GET');
 };
+
+// Handle Author delete on POST.
+exports.author_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete POST');
+};
