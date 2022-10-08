f182e99b8e91c21ec729d554e9e808f709ce3db2
Saugat Poudel <njsaugat@gmail.com>
2022-10-08 11:33:59 +0545

    controller added

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -203,3 +203,6 @@ router.get('/book/create', book_controller.book_create_get);
 
 // POST request for creating Book.
 router.post('/book/create', book_controller.book_create_post);
+
+// GET request to delete Book.
+router.get('/book/:id/delete', book_controller.book_delete_get);
