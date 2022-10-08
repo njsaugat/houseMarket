4acef44837fcf70009ca0d4885ee4ec05c579ddb
Saugat Poudel <njsaugat@gmail.com>
2022-10-08 11:35:18 +0545

    controller added

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -218,3 +218,6 @@ router.post('/book/:id/update', book_controller.book_update_post);
 
 // GET request for one Book.
 router.get('/book/:id', book_controller.book_detail);
+
+// GET request for list of all Book items.
+router.get('/books', book_controller.book_list);
