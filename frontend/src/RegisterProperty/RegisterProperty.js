d4537ee729025a310ea579c22ebed2a03bcb9027
Saugat Poudel <njsaugat@gmail.com>
2022-10-10 08:50:26 +0545

    register property updated

--- frontend/src/RegisterProperty/RegisterProperty.js
+++ frontend/src/RegisterProperty/RegisterProperty.js
@@ -5,16 +5,20 @@ import Mid from './Mid';
 import PrevNext from './PrevNext';
 import ProgressSteps from './ProgressSteps';
 
+const formItems = [<Intro />, <Mid />, <Final />];
 const RegisterProperty = () => {
   const [step, setStep] = useState(0);
   return (
-    <div>
+    <div className="font-poppins">
       <div className="left my-10">
         <ProgressSteps step={step} setStep={setStep} />
       </div>
       <form action="" className="flex justify-center">
-        <div className="right flex flex-col items-center justify-center shadow-lg rounded-lg border-slate-0 border-2 w-11/12 md:w-4/5 lg:w-1/2">
-          <Intro />
+        <div className="right flex flex-col items-center justify-center shadow-lg rounded-2xl border-slate-0 border-2 w-11/12 md:w-4/5 lg:w-1/2">
+          {formItems.map((formItem, index) => {
+            return index === step && formItem;
+          })}
+          {/* <Intro /> */}
           {/* <Mid /> */}
           {/* <Final /> */}
         </div>
