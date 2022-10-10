f4fd4a5f3d7ac0b54953d13cabaee1be3cacc3a5
Saugat Poudel <njsaugat@gmail.com>
2022-10-10 08:55:01 +0545

    mid updated

--- frontend/src/RegisterProperty/Mid.js
+++ frontend/src/RegisterProperty/Mid.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const buttonProperty = 'px-3 py-1  w-1/2 transition-all ease-out';
+const selectedProperty = `${buttonProperty} bg-gradient-to-t from-sky-400 to-cyan-100 font-bold tracking-wider `;
 
 const Mid = () => {
+  const [rentClicked, setRent] = useState(false);
   return (
     <>
       <div className="option flex flex-col my-5 self-center  w-1/2 ">
@@ -32,6 +36,35 @@ const Mid = () => {
           type="number"
           name="name"
         />
+      </div>
+      <div className=" flex my-5 self-center  w-full lg:w-10/12 bg-cyan-100 rounded-3xl p-1  ">
+        <button
+          className={
+            rentClicked
+              ? selectedProperty + ' rounded-tl-2xl rounded-bl-2xl'
+              : buttonProperty
+          }
+          onClick={(e) => {
+            e.preventDefault();
+
+            setRent(true);
+          }}
+        >
+          Rent
+        </button>
+        <button
+          className={
+            rentClicked
+              ? buttonProperty
+              : selectedProperty + ' rounded-tr-2xl rounded-br-2xl'
+          }
+          onClick={(e) => {
+            e.preventDefault();
+            setRent(false);
+          }}
+        >
+          Sale
+        </button>
       </div>
     </>
   );
