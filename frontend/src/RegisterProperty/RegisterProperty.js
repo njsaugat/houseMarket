8591b770ddf246534df6b18f9dd528aa9e419561
Saugat Poudel <njsaugat@gmail.com>
2022-10-11 17:29:45 +0545

    register property addedd

--- frontend/src/RegisterProperty/RegisterProperty.js
+++ frontend/src/RegisterProperty/RegisterProperty.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Banner from '../components/Banner';
 import Final from './Final';
 import Intro from './Intro';
 import Mid from './Mid';
@@ -8,19 +9,23 @@ import ProgressSteps from './ProgressSteps';
 const formItems = [<Intro />, <Mid />, <Final />];
 const RegisterProperty = () => {
   const [step, setStep] = useState(0);
+
+  // const [name,setName]=useState('')
+
   return (
-    <div className="font-poppins">
-      <div className="left my-10">
+    <div className=" bg-gradient-to-r from-gray-200 to-gray-50 h-screen">
+      {/* @TODO: add Navbar here */}
+      <h1 className="text-2xl font-bold text-center tracking-wider py-5 ">
+        Register your property at GharShar
+      </h1>
+      <div className="left py-10">
         <ProgressSteps step={step} setStep={setStep} />
       </div>
-      <form action="" className="flex justify-center">
-        <div className="right flex flex-col items-center justify-center shadow-lg rounded-2xl border-slate-0 border-2 w-11/12 md:w-4/5 lg:w-1/2">
-          {formItems.map((formItem, index) => {
-            return index === step && formItem;
+      <form action="" className="flex justify-center ">
+        <div className="right bg-white flex flex-col items-center justify-center shadow-lg rounded-2xl border-slate-0 border-2 w-11/12 md:w-4/5 lg:w-1/2 transition-all">
+          {formItems.map((FormItem, index) => {
+            return index === step && FormItem;
           })}
-          {/* <Intro /> */}
-          {/* <Mid /> */}
-          {/* <Final /> */}
         </div>
       </form>
       <PrevNext step={step} setStep={setStep} />
