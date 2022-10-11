3311ca445336264c5e174d45079fcd63181be3c2
Saugat Poudel <njsaugat@gmail.com>
2022-10-11 17:29:29 +0545

    mid file added

--- frontend/src/RegisterProperty/Mid.js
+++ frontend/src/RegisterProperty/Mid.js
@@ -4,42 +4,49 @@ const buttonProperty = 'px-3 py-1  w-1/2 transition-all ease-out';
 const selectedProperty = `${buttonProperty} bg-gradient-to-t from-sky-400 to-cyan-100 font-bold tracking-wider `;
 
 const Mid = () => {
-  const [isFurnished, setRent] = useState(false);
+  const [isFurnished, setIsFurnished] = useState(false);
   return (
     <>
-      <div className="option flex flex-col my-5 self-center  w-1/2 ">
-        <label className="font-bold tracking-wider mb-1" htmlFor="name">
+      <div className="option option2  flex items-center  my-5   w-3/5 ">
+        <label className="font-bold tracking-wider w-56 " htmlFor="bedrooms">
           Bedrooms
         </label>
         <input
-          className="border-gray-300 border-0 shadow-md  bg-gray-100 w-full  rounded-lg  px-3 py-2 outline-0 "
+          className=" border-gray-300 border-0 shadow-md ml-2 bg-gray-100 w-full lg:w-2/5 rounded-lg  px-3  outline-0 "
           type="number"
           name="name"
+          id="bedrooms"
+          min={0}
         />
       </div>
-      <div className="option flex flex-col my-5 self-center  w-1/2 ">
-        <label className="font-bold tracking-wider mb-1" htmlFor="name">
-          Living Rooms
+      <div className="option option2  flex items-center   my-5   w-3/5 ">
+        <label className="font-bold tracking-wider  w-56" htmlFor="living">
+          Living Room
         </label>
         <input
-          className="border-gray-300 border-0 shadow-md  bg-gray-100 w-full  rounded-lg  px-3 py-2 outline-0 "
+          className=" border-gray-300 border-0 shadow-md ml-2 bg-gray-100 w-full lg:w-2/5 rounded-lg  px-3  outline-0 "
           type="number"
           name="name"
+          id="living"
+          min={0}
         />
       </div>
-      <div className="option flex flex-col my-5 self-center  w-1/2 ">
-        <label className="font-bold tracking-wider mb-1" htmlFor="name">
+      <div className="option option2  flex items-center  my-5   w-3/5 ">
+        <label className="font-bold tracking-wider w-56" htmlFor="bathrooms">
           Bathrooms
         </label>
         <input
-          className="border-gray-300 border-0 shadow-md  bg-gray-100 w-full  rounded-lg  px-3 py-2 outline-0 "
+          className=" border-gray-300 border-0 shadow-md ml-2 bg-gray-100 w-full lg:w-2/5 rounded-lg  px-3  outline-0 "
           type="number"
           name="name"
+          min={0}
+          id="bathrooms"
         />
       </div>
-      <div className="flex flex-col my-5 self-center  w-1/2 ">
+
+      <div className="flex flex-col my-5 self-center  w-3/5 ">
         <p className="font-bold tracking-wider mb-1 self-start " htmlFor="name">
-          Select property type
+          Is the property furnished?
         </p>
         <div className=" flex my-5 self-center  w-full lg:w-10/12 bg-cyan-100 rounded-3xl p-1  ">
           <button
@@ -51,10 +58,10 @@ const Mid = () => {
             onClick={(e) => {
               e.preventDefault();
 
-              setRent(true);
+              setIsFurnished(true);
             }}
           >
-            Rent
+            Yes
           </button>
           <button
             className={
@@ -64,10 +71,10 @@ const Mid = () => {
             }
             onClick={(e) => {
               e.preventDefault();
-              setRent(false);
+              setIsFurnished(false);
             }}
           >
-            Sale
+            No
           </button>
         </div>
       </div>
