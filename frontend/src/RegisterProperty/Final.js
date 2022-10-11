0992336a071254f8e17dad7e7e066945661f2734
Saugat Poudel <njsaugat@gmail.com>
2022-10-11 17:28:54 +0545

    final file

--- frontend/src/RegisterProperty/Final.js
+++ frontend/src/RegisterProperty/Final.js
@@ -3,25 +3,40 @@ import React from 'react';
 const Final = () => {
   return (
     <>
-      <div className="option flex flex-col my-5 self-center  w-1/2 ">
-        <label className="font-bold tracking-wider mb-1" htmlFor="name">
+      <div className="option flex flex-col my-5 self-center  w-3/5 ">
+        <label className="font-bold tracking-wider mb-1" htmlFor="price">
+          Price
+        </label>
+        <input
+          className="border-gray-300 border-0 shadow-md  bg-gray-100 w-full  rounded-lg  px-3 py-2 outline-0 "
+          id="price"
+          type="number"
+          name="name"
+          min={0}
+          placeholder="Rs."
+        />
+      </div>
+      <div className="option flex flex-col my-5 self-center  w-3/5 ">
+        <label className="font-bold tracking-wider mb-1" htmlFor="address">
           Address
         </label>
         <input
           className="border-gray-300 border-0 shadow-md  bg-gray-100 w-full  rounded-lg  px-3 py-2 outline-0 "
           type="text"
           name="name"
+          id="address"
         />
       </div>
-      <div className="option flex flex-col my-5 self-center  w-1/2 ">
-        <label className="font-bold tracking-wider mb-1" htmlFor="name">
+      <div className="option flex flex-col my-5 self-center  w-3/5 ">
+        <label className="font-bold tracking-wider mb-1" htmlFor="description">
           Description
         </label>
-        <input
-          className="border-gray-300 border-0 shadow-md  bg-gray-100 w-full  rounded-lg  px-3 py-2 outline-0 "
+        <textarea
+          className="border-gray-300 border-0 shadow-md  bg-gray-100 w-full  rounded-lg  px-3 py-2 outline-0 resize-none h-28 "
           type="text"
           name="name"
-        />
+          id="description"
+        ></textarea>
       </div>
     </>
   );
