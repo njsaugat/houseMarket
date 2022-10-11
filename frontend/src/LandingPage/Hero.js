f25e963fa7820a73d6a62ced39b07098fdb11662
Saugat Poudel <njsaugat@gmail.com>
2022-10-11 17:30:59 +0545

    hero file updated

--- frontend/src/LandingPage/Hero.js
+++ frontend/src/LandingPage/Hero.js
@@ -4,7 +4,7 @@ import house from '../house.png';
 const Hero = () => {
   return (
     <main className="w-screen p-10 md:p-20 relative md:flex md:justify-between items-center overflow-hidden   mb-32  bg-gradient-to-r from-gray-200 to-gray-50 ">
-      <div className="left  my-24 mb-1  md:w-3/5">
+      <div className="left  my-24 mb-10  md:w-3/5">
         <Tagline textSize={'9xl'} />
         <section className="description text-gray-400 md:w-1/2">
           Our team of experts uses a methodology to identify the credit cards
