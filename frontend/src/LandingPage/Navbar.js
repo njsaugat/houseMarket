15601773ca7dba4d9d31931bf440058589191537
Saugat Poudel <njsaugat@gmail.com>
2022-10-12 17:32:30 +0545

    navbar updated

--- frontend/src/LandingPage/Navbar.js
+++ frontend/src/LandingPage/Navbar.js
@@ -18,7 +18,7 @@ const crossIcon = <FontAwesomeIcon icon={faXmark} />;
 
 const homeIcon = <FontAwesomeIcon icon={faHouse} />;
 const aboutUsIcon = <FontAwesomeIcon icon={faUser} />;
-const featuresIco = <FontAwesomeIcon icon={faListCheck} />;
+const featuresIcon = <FontAwesomeIcon icon={faListCheck} />;
 const solutionIcon = <FontAwesomeIcon icon={faCoins} />;
 
 const menuIcons = [homeIcon, aboutUsIcon, featuresIcon, solutionIcon];
