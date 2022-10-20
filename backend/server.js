100070d827e00071ef87208c620fd6eb970413f6
Saugat Poudel <njsaugat@gmail.com>
2022-10-20 11:25:43 +0545

    server file added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 5000;
-
+const cors = require('cors');
 app.get('/api', (req, res) => {
   // res.send('Hello ');
   res.json([
@@ -13,6 +13,13 @@ app.get('/api', (req, res) => {
     { brand: 'HP' },
   ]);
 });
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
