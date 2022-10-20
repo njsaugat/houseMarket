9890e37bca010d014074f2692c7204fd8e1b4cae
Saugat Poudel <njsaugat@gmail.com>
2022-10-20 11:06:07 +0545

    server file updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const PORT = 5000;
 
 app.get('/api', (req, res) => {
@@ -12,6 +12,14 @@ app.get('/api', (req, res) => {
     { brand: 'Lenovo' },
     { brand: 'HP' },
   ]);
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/formdata', (req, res) => {
+  console.log(req.body);
+  res.send('send');
 });
 
 app.listen(PORT, () => {
