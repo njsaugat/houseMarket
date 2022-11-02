3de2143695e60c854d8ca35e97762802acce4895
Saugat Poudel <njsaugat@gmail.com>
2022-11-02 10:55:26 +0545

    loading file added

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="kinetic-wrap w-screen h-screen flex justify-center items-center">
+      <div className="kinetic"></div>
+    </div>
+  );
+};
+
+export default Loading;
