69df17c8b861e641c30dc67dd8b25bcfa3b16ef4
Saugat Poudel <njsaugat@gmail.com>
2022-11-04 17:37:15 +0545

    login file added

--- frontend/src/Login-signup/Login.js
+++ frontend/src/Login-signup/Login.js
@@ -2,11 +2,26 @@ import React, { useRef } from 'react';
 import Banner from '../components/Banner';
 import Tagline from '../components/Tagline';
 import login from '../login.png';
+import Axios from 'axios';
 import validator, { checkEmail, checkPassword } from './validator';
 const Login = () => {
   const email = useRef(null);
   const password = useRef(null);
-
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // const
+    Axios.post('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'mutlipart/form-data',
+      },
+      body: {
+        email: email.current.value,
+        password: password.current.value,
+      },
+    });
+    //@TODO add promises to check whether the user is genuine or not based on response from server
+  };
   return (
     <div className=" flex flex-col md:flex-row  w-screen h-screen min-h-screen">
       <div className="hidden w-auto md:flex flex-col items-center  justify-center  bg-gradient-to-r from-blue-300  ">
@@ -24,7 +39,12 @@ const Login = () => {
         <h1 className="text-2xl font-bold mt-20 tracking-wider ">
           Login to GharShar
         </h1>
-        <form className="p-7 md:p-10  m-2 w-10/12 md:w-full   flex flex-col justify-center items-center">
+        <form
+          className="p-7 md:p-10  m-2 w-10/12 md:w-full   flex flex-col justify-center items-center"
+          onSubmit={(e) => {
+            onSubmit(e);
+          }}
+        >
           <div className="option flex flex-col my-5 self-center w-full lg:w-10/12">
             <label className="font-bold tracking-wider mb-1" htmlFor="email">
               Email
