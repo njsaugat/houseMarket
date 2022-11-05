2333d6d258191e745c191b57135de6f9b33899bb
Saugat Poudel <njsaugat@gmail.com>
2022-11-05 11:12:29 +0545

    property controller updates

--- backend/src/controllers/propertyController.js
+++ backend/src/controllers/propertyController.js
@@ -8,34 +8,59 @@ const prisma = require('../utils/prismaClient');
 
 exports.postProperty = async (req, res) => {
   console.log(req.body);
-  const {
-    name,
-    address,
-    description,
-    furnished,
-    price,
-    rent,
-    livingRooms,
-    bathrooms,
-    bedrooms,
-  } = req.body.body;
+  // const data = req.body.body;
+  // const {
+  //   name,
+  //   // address,
+  //   // description,
+  //   // furnished,
+  //   // price,
+  //   // rent,
+  //   // livingRooms,
+  //   // bathrooms,
+  //   // bedrooms,
+  // } = data;
+  // const title = req.body.body.name;
+  // const address = req.body.body.address;
+  // const description = req.body.body.description;
+  // const furnished = req.body.body.furnished;
+  // const price = req.body.body.price;
+  // const rent = req.body.body.rent;
+  // const livingRooms = req.body.body.livingRooms;
+  // const bathrooms = req.body.body.bathrooms;
+  // const bedrooms = req.body.body.bedrooms;
 
-  const newProperty = await prisma.property.create({
-    data: {
-      name: name,
-      location: address,
-      description: description,
-      furnished: furnished,
-      price: price,
-      propertyType: rent === true ? 'Rent' : 'Sale',
-      livingRoom: livingRooms,
-      bathRoom: bathrooms,
-      bedRoom: bedrooms,
-    },
-  });
+  // console.log(name);
 
   if (!req.file) {
     console.log('No file upload');
+    const {
+      name,
+      address,
+      description,
+      furnished,
+      price,
+      rent,
+      livingRooms,
+      bathrooms,
+      bedrooms,
+    } = req.body.body;
+    const newProperty = await prisma.property.create({
+      data: {
+        name: name,
+        location: address,
+        description: description,
+        furnished: furnished,
+        price: +price,
+        propertyType: rent === true ? 'Rent' : 'Sale',
+        livingRoom: +livingRooms,
+        bathRoom: +bathrooms,
+        bedRoom: +bedrooms,
+        owner: {},
+        images: {},
+        // ownerId: ' ',
+      },
+    });
   } else {
     console.log(req.file.filename);
     const imgsrc = 'http://127.0.0.1:5000/images/' + req.file.filename;
