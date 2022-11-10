bdca6dea6996537d22f56a312ded78b71994ec03
Saugat Poudel <njsaugat@gmail.com>
2022-11-10 10:29:04 +0545

    user controller file added

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,15 +1,18 @@
 const prisma = require('../utils/prismaClient');
-
+const bcrypt = require('bcrypt');
+// const crypto=require('crypto');
 exports.postOwner = async (req, res) => {
   const { name, email, password } = req.body.body;
   //   console.log(typeof name, typeof email, typeof password + '\n');
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
   const newProperty = await prisma.owner.create({
     data: {
       //   id: '23324',
       name: name,
       username: name,
       email: email,
-      password: password,
+      password: hashedPassword,
       properties: {},
     },
   });
