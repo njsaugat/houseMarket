5803c58975cce292c2ce983ef873f76646fb520e
Saugat Poudel <njsaugat@gmail.com>
2022-11-14 11:20:17 +0545

    banner updates

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 const Banner = () => {
   return (
-    <div className="logo w-3/5  flex items-center font-bold text-4xl my-5  leading-tight md:leading-snug md:w-1/2">
-      <img
-        // className="w-1/4 h-1/4 md:w-1/6 md:h-1/6"
-        className="w-20 h-20"
-        src={logo}
-        alt=""
-        srcSet=""
-      />
-      <span>Ghar</span>
-      <span className="text-cyan-400">Shar</span>
-    </div>
+    <Link to="/">
+      <div className="logo w-3/5  flex items-center font-bold text-4xl my-5  leading-tight md:leading-snug md:w-1/2">
+        <img
+          // className="w-1/4 h-1/4 md:w-1/6 md:h-1/6"
+          className="w-20 h-20"
+          src={logo}
+          alt=""
+          srcSet=""
+        />
+        <span>Ghar</span>
+        <span className="text-cyan-400">Shar</span>
+      </div>
+    </Link>
   );
 };
 
