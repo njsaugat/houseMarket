39e54d9ad31aa51143f4d9d8b765e1d570122fe3
Saugat Poudel <njsaugat@gmail.com>
2022-11-16 11:41:34 +0545

    explore file added

--- frontend/src/explore/Explore.js
+++ frontend/src/explore/Explore.js
@@ -4,13 +4,11 @@ import Loading from '../components/Loading';
 import Navbar from '../LandingPage/Navbar';
 import ShowPage from './ShowPage';
 import { Link } from 'react-router-dom';
+import LinkProperty from '../components/LinkProperty';
+import Footer from '../LandingPage/Footer';
 
 const LETTERS_MAX_LENGTH = 100;
 const buttonProperty = `py-2 px-5 my-10 rounded-lg bg-gradient-to-t from-sky-400 to-cyan-100  tracking-wide text-black shadow-lg cursor-pointer`;
-
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
 
 const PROPERTIES_PER_PAGE = 1;
 
@@ -60,50 +58,7 @@ const Explore = () => {
       <div className="properties w-screen h-screen bg-slate-100 flex flex-col  items-center transition-all ">
         {properties.map((property, index) => {
           return (
-            returnIndexRange(index) && (
-              <Link
-                className="property flex  bg-slate-50 m-5 p-8 gap-4 w-11/12 md:w-3/5 lg:w-2/5 rounded-xl shadow-lg hover:shadow-xl hover:bg-white   transition-shadow   duration-300 "
-                key={property.id}
-                to={`/explore/property/${property.id}`}
-                state={property}
-              >
-                <div className="image w-1/3">
-                  <img
-                    className="rounded-3xl object-cover opacity-95 hover:scale-105 transition-transform w-full h-full  "
-                    src={
-                      'http://127.0.0.1:5000/' +
-                      property.imageUrl.substring(
-                        property.imageUrl.indexOf('photo')
-                      )
-                    }
-                    alt=""
-                    srcSet=""
-                    loading="lazy"
-                  />
-                </div>
-                <div className="h-full w-2/3">
-                  <h1 className="font-bold tracking-wide text-lg capitalize">
-                    {property.name}
-                  </h1>
-                  <p className="text-slate-500 text-sm leading-4 my-3">
-                    {property.description.substring(0, LETTERS_MAX_LENGTH) +
-                      '...'}
-                  </p>
-                  <div className="attributes flex items-center gap-5 gap-x-20 ">
-                    <span className="price text-lg font-bold">
-                      रु {numberWithCommas(property.price)}
-                    </span>
-                    <span className="type">{property.propertyType}</span>
-                  </div>
-                  {/* <div className="location text-slate-500 text-sm mt-1">
-                    {' '}
-                    {property.location}
-                  </div> */}
-                  <hr className="my-3" />
-                  <div className="owner mt-2">{property.location}</div>
-                </div>
-              </Link>
-            )
+            returnIndexRange(index) && <LinkProperty property={property} />
           );
         })}
         <div className="pagination  bg-slate-100 flex gap-6 items-center">
@@ -143,6 +98,7 @@ const Explore = () => {
           </span>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
