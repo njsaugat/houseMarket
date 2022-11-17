a1c1d0e71b1766b031caff504bae6e6effca12e8
Saugat Poudel <njsaugat@gmail.com>
2022-11-17 12:29:31 +0545

    property controller added

--- backend/src/controllers/propertyController.js
+++ backend/src/controllers/propertyController.js
@@ -1,5 +1,6 @@
 // const { prisma } = require('@prisma/client');
 const express = require('express');
+const { property } = require('../utils/prismaClient');
 const router = express.Router();
 
 // import { prisma } from '../utils/prismaClient';
@@ -10,7 +11,7 @@ let imagePath;
 //if we have multiple images then that would probably be stored in array
 // so we have to make the imagePath also to be in array so that we could retrieve them
 exports.postProperty = async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
   if (!req.file) {
     console.log('No file upload');
@@ -93,4 +94,50 @@ exports.getProperty = async (req, res) => {
   // res.send(allProperties);
   res.send(allProperties);
   // return properties;
+};
+
+exports.putProperty = async (req, res) => {
+  console.log(req.body);
+  const {
+    name,
+    address,
+    description,
+    furnished,
+    price,
+    rent,
+    livingRooms,
+    bathrooms,
+    bedrooms,
+    userId,
+    propertyId,
+  } = req.body.body;
+  const updateProperty = await prisma.property.update({
+    where: {
+      id: propertyId,
+    },
+    data: {
+      name: name,
+      location: address,
+      description: description,
+      furnished: furnished,
+      price: +price,
+      propertyType: rent === true ? 'Rent' : 'Sale',
+      livingRoom: +livingRooms,
+      bathRoom: +bathrooms,
+      bedRoom: +bedrooms,
+      // owner: findUser(userId),
+      // owner: { connect: { id: userId } },
+    },
+  });
+  console.log(updateProperty);
+};
+
+exports.deleteProperty = async (req, res) => {
+  const propertyId = req.params.id;
+  console.log(propertyId);
+  await prisma.property.delete({
+    where: {
+      id: propertyId,
+    },
+  });
 };
