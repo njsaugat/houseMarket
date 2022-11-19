9666dc2b5b700edfa4a70f7b01091af42402031a
Saugat Poudel <njsaugat@gmail.com>
2022-11-19 12:41:28 +0545

    loading file added

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Footer from '../LandingPage/Footer';
+import Navbar from '../LandingPage/Navbar';
+import Loads from './Loads';
 
 const Loading = () => {
+  // const [Loading, setLoading] = useState(<Loads />);
+  // const ShowLoading = () => {
+  //   return (
+  //     <>
+  //       <Navbar />
+  //       <Loads />
+  //       <Footer />
+  //     </>
+  //   );
+  // };
+  // setTimeout(() => {
+  //   return <ShowLoading />;
+  // }, 2000);
+
+  // useEffect(() => {
+  //   setTimeout(setLoading(<ShowLoading />), 2000);
+  // }, []);
+  // return Loading;
+
   return (
-    <div className="kinetic-wrap w-screen h-screen flex justify-center items-center">
-      <div className="kinetic"></div>
-    </div>
+    <>
+      <Navbar />
+      <Loads />
+      <Footer />
+    </>
   );
+  // return (
+
+  // );
 };
 
 export default Loading;
