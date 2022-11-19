110f7853d2de0e8967f419f47c3e90c7ceb4f65a
Saugat Poudel <njsaugat@gmail.com>
2022-11-19 12:41:39 +0545

    loads file added

---
+++ frontend/src/components/Loads.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loads = () => {
+  return (
+    <div className="kinetic-wrap w-screen h-screen flex justify-center items-center">
+      <div className="kinetic"></div>
+    </div>
+  );
+};
+
+export default Loads;
