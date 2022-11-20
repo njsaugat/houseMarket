502765d88e30ec967dddec2c46e9616c17bc2731
Saugat Poudel <njsaugat@gmail.com>
2022-11-20 08:16:54 +0545

    navbar file added

--- frontend/src/LandingPage/Navbar.js
+++ frontend/src/LandingPage/Navbar.js
@@ -47,10 +47,10 @@ const Navbar = () => {
     }, []);
     return createPortal(
       <div
-        className={`fixed navs top-0 left-0 right-0 bottom-0 z-100 bg-gradient-to-b from-cyan-600 to-cyan-100  bg-opacity-95 overflow-y-hidden
-      w-11/12 h-screen text-white  transition ease-in-out delay-400 inset-0`}
+        className={`fixed navs top-0 left-0 right-0 bottom-0 z-100 bg-gradient-to-b from-cyan-600 to-sky-800  bg-opacity-95 overflow-y-hidden
+      w-screen h-screen text-white  transition-all ease-in-out delay-400 inset-0`}
       >
-        <div className="absolute    flex justify-between w-11/12 h-screen z-100    text-2xl  p-10 ">
+        <div className="absolute  transition-all  flex justify-between w-11/12 h-screen z-100    text-2xl  p-10 ">
           <div className="lists flex flex-col gap-10">
             <RenderMenuList icons={menuIcons} />
           </div>
@@ -59,7 +59,7 @@ const Navbar = () => {
               setShowMenu(false);
               portal.classList.remove('active');
             }}
-            className="cursor-pointer rounded-lg  p-2 bg-gradient-to-b from-gray-600 to-black-50 w-10 h-10 flex justify-center items-center"
+            className="cursor-pointer rounded-lg  p-2 bg-gradient-to-b from-slate-400  to-slate-50 w-10 h-10 flex justify-center items-center text-black hover:shadow-xl hover:rotate-90 transition-all duration-300"
           >
             {crossIcon}
           </span>
@@ -69,20 +69,23 @@ const Navbar = () => {
     );
   };
   return (
-    <nav className="flex  items-center space-x-2 px-10 md:px-20 w-screen gap-10 md:gap-32 lg:gap-1  relative navbar ">
+    <nav className="flex font-Poppins items-center space-x-2 px-10 md:px-20 w-screen gap-10 md:gap-32 lg:gap-1  relative navbar ">
       <Banner />
       <div
-        className="lg:hidden absolute right-10  text-3xl cursor-pointer rounded-lg  p-2 bg-gradient-to-b from-gray-600 to-black-50"
+        className="lg:hidden absolute right-4 md:right-10  text-3xl cursor-pointer rounded-lg  p-2 hover:rotate-180 hover:origin-center transition-all duration-300 "
         onClick={() => {
           setShowMenu(true);
           portal.classList.add('active');
         }}
       >
-        {menuIcon}
+        <span className="h-full  hover:rotate-180 transition-all duration-300">
+          {menuIcon}
+        </span>
+        {/* {menuIcon} */}
       </div>
       {showMenu && <ShowPortal />}
       <ul
-        className={`hidden absolute -right-1/4 lg:flex lg:gap-10  md:w-11/12 lg:w-3/5 text-xl  items-center my-3`}
+        className={`hidden absolute -right-1/4 lg:flex lg:gap-10  md:w-11/12 lg:w-4/6  text-lg items-center my-0 `}
       >
         <RenderMenuList />
       </ul>
