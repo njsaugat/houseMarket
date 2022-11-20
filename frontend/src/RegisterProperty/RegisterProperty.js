68604dfa3d060ac99b2da6c66dc47753880f69b4
Saugat Poudel <njsaugat@gmail.com>
2022-11-20 08:18:09 +0545

    register property

--- frontend/src/RegisterProperty/RegisterProperty.js
+++ frontend/src/RegisterProperty/RegisterProperty.js
@@ -1,6 +1,12 @@
-import React, { createContext, useEffect, useRef, useState } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 // import Banner from '../components/Banner';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Final from './Final';
 import Intro from './Intro';
 import Mid from './Mid';
@@ -8,70 +14,112 @@ import Mid from './Mid';
 import ProgressSteps from './ProgressSteps';
 import Axios from 'axios';
 import Loading from '../components/Loading';
+import { LoggedInContext } from '../App';
+import Navbar from '../LandingPage/Navbar';
+import Footer from '../LandingPage/Footer';
 export const FormContext = createContext();
 const formItems = [<Intro />, <Mid />, <Final />];
 const RegisterProperty = () => {
   document.title = 'GharShar | Register Property';
-
+  const { state } = useLocation();
+  console.log(state);
+  const property = state;
   const navigate = useNavigate();
   const [step, setStep] = useState(0);
 
-  const [userData, setUserData] = useState({});
-  const [rentClicked, setRent] = useState(false);
-  const [isFurnished, setIsFurnished] = useState(false);
-  const [showImage, setShowImage] = useState(false);
+  // const [userData, setUserData] = useState({});
+  const [rentClicked, setRent] = useState(
+    property ? property.propertyType.toLowerCase() === 'rent' && true : false
+  );
+  const [isFurnished, setIsFurnished] = useState(
+    property ? property.furnished : false
+  );
+  const [showImage, setShowImage] = useState(
+    property ? property.imageUrl && true : false
+  );
   const previewImage = useRef(null);
   const inputImageFile = useRef(null);
   // const [selectedImage, setSelectedImage] = useState(null);
   const [image, setImage] = useState({ preview: '', data: '' });
-  const [isLoading, setLoading] = useState(true);
+  // const [isLoading, setLoading] = useState(true);
   // const [name,setName]=useState('')
+
+  const { loggedIn } = useContext(LoggedInContext);
+
   const [formState, setState] = useState({
-    name: '',
-    imageUrl: '',
-    bedrooms: '',
-    livingRooms: '',
-    bathrooms: '',
-    price: '',
-    address: '',
-    description: '',
+    name: property ? property.name : '',
+    imageUrl: property
+      ? 'http://127.0.0.1:5000/' +
+        property.imageUrl.substring(property.imageUrl.indexOf('photo'))
+      : '',
+    bedrooms: property ? property.bedRoom : '',
+    livingRooms: property ? property.livingRoom : '',
+    bathrooms: property ? property.bathRoom : '',
+    price: property ? property.price : '',
+    address: property ? property.location : '',
+    description: property ? property.description : '',
     image: '',
   });
+  console.log(formState);
+  // if (state) {
+  //   setState(state);
+  // }
   // const [userId, setUserId] = useState('');
 
-  useEffect(() => {
-    // const userID = localStorage.getItem('userId');
-    // setUserId(userID);
-    // console.log('user id is ' + userId);
-    async function getUserData() {
-      const data = await fetch('/register-property');
-      const uData = await data.json();
-      console.log(uData);
-      setUserData(uData);
-      setLoading(false);
-      // return uData;
-    }
+  // useEffect(() => {
+  //   // const userID = localStorage.getItem('userId');
+  //   // setUserId(userID);
+  //   // console.log('user id is ' + userId);
+  //   async function getUserData() {
+  //     const data = await fetch('/register-property');
+  //     const uData = await data.json();
+  //     console.log(uData);
+  //     // setUserData(uData);
+  //     setLoading(false);
+  //     // return uData;
+  //   }
 
-    // setUserData(getUserData());
-    getUserData();
+  //   // setUserData(getUserData());
+  //   getUserData();
 
-    // if (items) {
-    //  setItems(items)
-    // }
-  }, []);
+  //   // if (items) {
+  //   //  setItems(items)
+  //   // }
+  // }, []);
 
-  console.log(Object.keys(userData).length === 0);
-  if (isLoading) {
-    // return navigate('/loading');
-    return <Loading />;
-  }
-  if (!userData.loggedIn) {
+  // console.log(Object.keys(userData).length === 0);
+
+  // if (isLoading) {
+  //   // return navigate('/loading');
+  //   return <Loading />;
+  // }
+  if (!loggedIn) {
     return navigate('/login');
   }
 
   function submit(e) {
     e.preventDefault();
 
+    if (property != null && property.update != null) {
+      //@TODO: check if image has been updated
+      Axios.put('/formdata', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'mutlipart/form-data',
+        },
+        body: {
+          ...formState,
+          rent: rentClicked,
+          furnished: isFurnished,
+          // userId: userData.userId,
+          propertyId: property.id ? property.id : '',
+        },
+      });
+      navigate('/loading');
+      return setTimeout(() => {
+        navigate('/');
+      }, 2000);
+    }
     const formData = new FormData();
     formData.append('photo', image.data, image.data.name);
 
@@ -86,7 +134,7 @@ const RegisterProperty = () => {
         ...formState,
         rent: rentClicked,
         furnished: isFurnished,
-        userId: userData.userId,
+        // userId: userData.userId,
       },
     });
 
@@ -115,6 +163,7 @@ const RegisterProperty = () => {
         // selectedImage,setSelectedImage
       }}
     >
+      <Navbar />
       <div className=" bg-gradient-to-r from-gray-200 to-gray-50 h-screen">
         {/* //@TODO: add Navbar here  */}
         <h1 className="text-2xl font-bold text-center tracking-wider py-5 ">
@@ -146,6 +195,7 @@ const RegisterProperty = () => {
         </form>
         {/* <PrevNext step={step} setStep={setStep} /> */}
       </div>
+      <Footer />
     </FormContext.Provider>
   );
 };
