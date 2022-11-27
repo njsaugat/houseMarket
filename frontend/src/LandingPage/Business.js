0b55169b88dc4dd4fe010a2b1f825b57a3b45739
Saugat Poudel <njsaugat@gmail.com>
2022-11-27 12:39:29 +0545

    business stuff updated

--- frontend/src/LandingPage/Business.js
+++ frontend/src/LandingPage/Business.js
@@ -4,7 +4,7 @@ import {
   faHouseSignal,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const houseIcon = <FontAwesomeIcon icon={faHouse} />;
 const businessIcon = <FontAwesomeIcon icon={faBusinessTime} />;
@@ -33,9 +33,24 @@ const sellingFactors = [
   },
 ];
 const Business = () => {
+  useEffect(() => {
+    const contents = document.querySelectorAll('.content');
+    console.log(contents);
+    function showBox() {
+      const triggerBottom = (window.innerHeight / 5) * 4; //we are setting the trigger point; as we enter here event should fire up
+      contents.forEach((content) => {
+        const boxTop = content.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) {
+          content.classList.add('show');
+        }
+      });
+    }
+    window.addEventListener('scroll', showBox);
+    return () => window.removeEventListener('scroll', showBox);
+  });
   const renderSellingFactors = sellingFactors.map((sellingFactor) => {
     let initialStyle =
-      'py-5 rounded-md  flex items-center gap-5 w-10/12 my-10 md:mx-5 shadow-lg p-5';
+      ' content py-5 rounded-md  flex items-center gap-5 w-10/12 my-10 md:mx-5 shadow-lg p-5';
     let computedStyle = 'bg-gradient-to-b from-gray-300 to-transparent-500';
     initialStyle += sellingFactor.id === 2 ? ' ' + computedStyle : '';
     // initialStyle += computedStyle  '';
